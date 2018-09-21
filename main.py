9fc5aa818aaba6c3f5ee9ca5824eee70e07cbef7
William Park <williamdparkk@gmail.com>
2018-09-21 12:04:22 -0500

    initial model generation with local file

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 Store_all = []
-with open("3nvf.pdb") as protein:
+with open("protein_model/3nvf.pdb") as protein:
     for lines in protein:
         if "ATOM   " in lines:
             lines = lines.split()
             #'ATOM', '1', 'N', 'LEU', 'A', '125', '4.329', '-12.012', '2.376', '1.00', '0.00', 'N'
             Store_all.append(list(map(float, lines[6:9])))
 
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
 x,y,z = list(zip(*Store_all))
 
 fig = plt.figure()
 ax = Axes3D(fig)
 
-ax.plot(x,y,z, "o")
+ax.scatter(x,y,z, s=1200, edgecolors="black", alpha = 0.7)
+#markeredgecolor="black"
 ax.axis("off")
 
 plt.show()
